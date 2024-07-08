9f6692c13f25b528de478891affe746c54016b7b
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-08 20:20:27 +0300

    Initial Commit

---
+++ src/schoolmanagement/system/Student.java
@@ -0,0 +1,29 @@
+package schoolmanagement.system;
+
+/**
+ * Created by Samuella on 8/7/2024
+ * This class is responsible for keeping the
+ * track of students fees, name, grade and fees
+ * paid.
+ */
+
+public class Student {
+
+    //fields
+   private int id;
+   private String name;
+   private int grade;
+   private int feesPaid;
+   private int feesTotal;
+
+    /**
+     *To create a new student by initialising
+     * @param id id for the student: unique.
+     * @param name name of the student.
+     * @param grade grade of the student.
+     */
+
+    public student(int id, String name, int grade){
+
+    }
+}
